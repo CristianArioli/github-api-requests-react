32dbc86e178ecc1619f2c1d0504dda69cc1e999a
Cristian <arioli.cristian@gmail.com>
2021-03-07 14:05:09 -0300

    Primeira versão tela de login e tela de search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import LoginForm from './components/Login';
+import SearchForm from './components/Search';
+import { GlobalStyle } from './components/styles';
 
 function App() {
+  const adminUser = {
+    email: 'admin@admin.com',
+    password: 'admin123',
+  };
+
+  const [user, setUser] = useState({ email: '' });
+  const [error, setError] = useState('');
+
+  const Login = (details) => {
+    if (details.email === adminUser.email && details.password === adminUser.password) {
+      localStorage.setItem('@login-app/email', details.email);
+      setUser({
+        email: details.email,
+      });
+    } else {
+      setError('Details do not match!');
+    }
+  };
+
+  const Logout = () => {
+    localStorage.removeItem('@login-app/email');
+    setUser({ email: '' });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      {(user.email !== '') ? (
+        <div>
+          <SearchForm />
+          <button onClick={Logout} type="button">teste</button>
+        </div>
+      ) : (
+        <LoginForm loginFn={Login} error={error} />
+      )}
     </div>
   );
 }
